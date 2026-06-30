61e9a7f94e333c6aae5da76661ea5c6e087a9cba
Oliver Rockstedt <info@sourcebox.de>
2026-06-30 14:04:30 +0200

    Moved pick lists into sub directory

--- src/ui/elements/mod.rs
+++ src/ui/elements/mod.rs
@@ -1,15 +1,8 @@
 //! Individual control elements used in various places
 
-pub mod arp_grid_list;
-pub mod arp_mode_list;
+mod pick_lists;
+
 pub mod checkbox;
-pub mod env_trigger_list;
-pub mod fx_mode_list;
-pub mod lfo_phase_list;
-pub mod lfo_shape_list;
-pub mod midi_channel_list;
-pub mod mod_target_list;
-pub mod part_list;
-pub mod shaper_mode_list;
 pub mod slider;
-pub mod wavetable_list;
+
+pub use pick_lists::*;
---
+++ src/ui/elements/pick_lists/mod.rs
@@ -0,0 +1,13 @@
+//! Dropdown lists.
+
+pub mod arp_grid_list;
+pub mod arp_mode_list;
+pub mod env_trigger_list;
+pub mod fx_mode_list;
+pub mod lfo_phase_list;
+pub mod lfo_shape_list;
+pub mod midi_channel_list;
+pub mod mod_target_list;
+pub mod part_list;
+pub mod shaper_mode_list;
+pub mod wavetable_list;
